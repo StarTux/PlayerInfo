7c1538525e5c92571d25b818b7be587cb3747fb7
Thomas Loy <ths.loy@googlemail.com>
2015-03-31 03:18:07 +0200

    Use PlayerJoinEvent instead of PlayerLoginEvent to allow easy permission check

--- src/main/java/com/winthier/playerinfo/bukkit/BukkitEventHandler.java
+++ src/main/java/com/winthier/playerinfo/bukkit/BukkitEventHandler.java
@@ -6,16 +6,16 @@ import lombok.RequiredArgsConstructor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 @RequiredArgsConstructor
 class BukkitEventHandler implements Listener {
     private final BukkitPlayerInfoPlugin plugin;
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerLogin(PlayerLoginEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         final UUID uuid = event.getPlayer().getUniqueId();
-        final String ip = event.getAddress().toString().substring(1);
+        final String ip = event.getPlayer().getAddress().getAddress().toString().substring(1);
         PlayerInfo.getInstance().onPlayerLogin(uuid, ip);
     }
 }
